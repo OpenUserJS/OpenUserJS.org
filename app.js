8bfeeab8bdae5dc8d170553bcd3f6bd963f74d91
sizzlemctwizzle <medleymind@gmail.com>
2013-11-08 19:53:29 -0600

    Setup basic login and session management of a single hardcoded user account for #1.

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var app = express();
 var controllers = require('./controllers');
+var settings = require('./models/settings.json');
 
 app.configure(function(){
   app.use(express.urlencoded());
   app.use(express.json());
   app.use(express.compress());
   app.use(express.methodOverride());
+
+  // Order is very important here (i.e mess with at your own risk)
+  app.use(express.cookieParser(settings.secret));
+  app.use(express.session());
   app.use(app.router);
 });
 
@@ -17,6 +22,8 @@ app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 
 app.get('/', controllers.home);
+app.get('/logout/', controllers.logout);
+app.post('/', controllers.login);
 
 app.use(express.static(__dirname + '/public'));
 app.use(function(req, res, next){
--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,18 @@
+var User = require('../models/user').User;
+
 exports.home = function(req, res) {
-  res.render('index', { 'title': 'Home page' }, res);
+  var user = new User(req.session);
+  res.render('index', { 'title': 'Home page', 'user': user.settings }, res);
+}
+
+exports.login = function(req, res) {
+  var user = new User(req.session);
+  user.login(req.body.username, req.body.password);
+  res.redirect('/');
+}
+
+exports.logout = function(req, res) {
+  var user = new User(req.session);
+  user.logout();
+  res.redirect('/');
 }
