d267a80334b42a09faa0bc38ac3f8c2da8684919
simonzack <simonzack@gmail.com>
2013-11-12 22:24:40 +1100

    moved configuration in app.js to config.js; added mongoose configuration

--- app.js
+++ app.js
@@ -1,24 +1,8 @@
+
 var express = require('express');
 var app = express();
 var controllers = require('./controllers');
+var mongoose = require('mongoose');
+var config = require('./config.js')(app, express, controllers, mongoose);
 
-app.configure(function(){
-  app.use(express.urlencoded());
-  app.use(express.json());
-  app.use(express.compress());
-  app.use(express.methodOverride());
-  app.use(app.router);
-});
-
-app.listen(8080);
-
-app.engine('html', require('./libs/muExpress').renderFile);
-app.set('view engine', 'html');
-app.set('views', __dirname + '/views');
-
-app.get('/', controllers.home);
-
-app.use(express.static(__dirname + '/public'));
-app.use(function(req, res, next){
-  res.sendfile(__dirname + '/public/404.html');
-});
+app.listen(process.env.port || 8080);
