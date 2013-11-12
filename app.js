e2137c498c26e87a8d1322e3583201b3e7c82812
simonzack <simonzack@gmail.com>
2013-11-12 22:28:50 +1100

    added the user model

--- app.js
+++ app.js
@@ -4,5 +4,7 @@ var app = express();
 var controllers = require('./controllers');
 var mongoose = require('mongoose');
 var config = require('./config.js')(app, express, controllers, mongoose);
+var models = {};
+models.user = require('./models/user')(mongoose.conn).model;
 
 app.listen(process.env.port || 8080);
