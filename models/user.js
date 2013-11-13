caab06b522e42a510af9862a68f02ee0ebf17b86
sizzlemctwizzle <medleymind@gmail.com>
2013-11-12 18:00:43 -0600

    Remove leftover code.

--- models/user.js
+++ models/user.js
@@ -14,34 +14,4 @@ var userSchema = new Schema({
 var User = mongoose.model('User', userSchema);
 
 exports.User = User;
-/*
-function User(session) {
-  this.settings = session.user || {};
-  this.session = session;
-}
 
-User.prototype = {
-  login: function(username, password) {
-    // Replace with a DB lookup
-    if (username == "user" && password === "pass")
-    {
-      this.settings = {
-        loggedIn: true, 
-        username: username
-      }
-
-      this.session.user = this.settings;
-    }
-  },
-
-  logout: function() {
-    delete this.session.user;
-    this.settings = {};
-  },
-
-  register: function() {
-    // do registration jazz here
-    // maybe a seperate model since it's complex?
-  }
-};
-*/
