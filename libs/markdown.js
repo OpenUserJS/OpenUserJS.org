69a0baaba7301e672fec6f214a0aad70aa3fb9e5
sizzlemctwizzle <medleymind@gmail.com>
2014-03-31 15:09:10 -0500

    Bugfix for getting the anchor text of a more complicated header.

--- libs/markdown.js
+++ libs/markdown.js
@@ -1,12 +1,33 @@
 var md = require('markdown').markdown;
 
+function getHeaderText (el) {
+  var i = 0;
+  var text = '';
+
+  if (el instanceof Array) {
+    if (el.length === 2) {
+      text = getHeaderText(el[1]);
+    } else {
+      if (el[0] === 'header' || el[0] === 'link') { ++i; }
+      for (; i < el.length; ++i) {
+        text += getHeaderText(el[i]);
+      }
+    }
+  } else if (typeof el === 'string') {
+    text = el;
+  }
+
+  return text;
+}
+
 exports.renderMd = function (text) {
   var tree = md.parse(text);
 
-  tree.forEach(function (el) {
-    if (el instanceof Array && el[0] === 'header') {
-      el[1]['id'] = 'anchor-' + el[2].replace(/\s+/g, '-')
-        .replace(/"/g, '');
+  tree.forEach(function (header) {
+    var anchorId = null;
+    if (header instanceof Array && header[0] === 'header') {
+      header[1]['id'] = 'anchor-' + getHeaderText(header)
+        .replace(/\s+/g, '-').replace(/"/g, '');
     }
   });
   
