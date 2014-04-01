4662e4e58dbaaedda43af0d4d11c07b0d681d94c
sizzlemctwizzle <medleymind@gmail.com>
2014-04-01 18:58:23 -0500

    Switch to using GitHub Flavored Markdown and syntax highlighting for code blocks. Closes #22

--- libs/markdown.js
+++ libs/markdown.js
@@ -1,30 +1,37 @@
-var md = require('markdown').markdown;
+var marked = require('marked');
+var hljs = require('highlight.js');
+var renderer = new marked.Renderer();
 
-function getHeaderText (el) {
-  var i = 1;
-  var text = '';
+renderer.heading = function (text, level) {
+  var escapedText = text.toLowerCase().replace(/<\/?[^>]+?>/g, '')
+    .replace(/[^\w]+/g, '-');
 
-  if (el instanceof Array) {
-    for (; i < el.length; ++i) {
-      text += getHeaderText(el[i]);
+  return '<h' + level + '><a name="anchor-' +
+    escapedText +
+    '" class="anchor" href="#anchor-' +
+    escapedText +
+    '"><span class="header-link"></span></a>' +
+    text + '</h' + level + '>';
+};
+
+marked.setOptions({
+  highlight: function (code, lang) {
+    if (lang) {
+      return hljs.highlight(lang, code).value;
+    } else {
+      return hljs.highlightAuto(code).value;
     }
-  } else if (typeof el === 'string') {
-    text = el;
-  }
-
-  return text;
-}
+  },
+  renderer: renderer,
+  gfm: true,
+  tables: true,
+  breaks: true,
+  pedantic: false,
+  sanitize: true,
+  smartLists: true,
+  smartypants: false
+});
 
 exports.renderMd = function (text) {
-  var tree = md.parse(text);
-
-  tree.forEach(function (header) {
-    var anchorId = null;
-    if (header instanceof Array && header[0] === 'header') {
-      header[1]['id'] = 'anchor-' + getHeaderText(header)
-        .replace(/\s+/g, '-').replace(/"/g, '');
-    }
-  });
-  
-  return md.renderJsonML(md.toHTMLTree(tree));
+  return marked(text);
 };
