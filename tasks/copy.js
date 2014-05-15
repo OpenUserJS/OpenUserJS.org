b38b961a094d14fead15b09f27c898447fecdcd2
frank@solygen.de <frank@solygen.de>
2014-05-15 18:53:50 +0200

    fix indent size

--- tasks/copy.js
+++ tasks/copy.js
@@ -1,61 +1,61 @@
 module.exports = {
-    main: {
-        files: [
+  main: {
+    files: [
 
-            //root files
-            {
-                expand: true,
-                cwd: '',
-                src: 'app.js',
-                dest: 'builds/'
-            },
-            {
-                expand: true,
-                cwd: '',
-                src: 'fakes3.sh',
-                dest: 'builds/'
-            },
-            //libs
-            {
-                expand: true,
-                cwd: 'node_modules/',
-                src: '**/lib/*.js',
-                dest: 'builds/node_modules/'
-            },
-            {
-                expand: true,
-                cwd: 'libs/',
-                src: '**/*',
-                dest: 'builds/libs/'
-            },
-            //public
-            //css in tasks/cssmin.js
-            {
-                expand: true,
-                cwd: 'public/images',
-                src: '**/*',
-                dest: 'builds/public/images'
-            },
-            {
-              expand: true,
-              cwd: 'public/js',
-              src: '**/*.js',
-              dest: 'builds/public/js'
-            },
-            //mvc
-            {
-                expand: true,
-                cwd: 'controllers/',
-                src: '**/*',
-                dest: 'builds/controllers/'
-            },
-            {
-                expand: true,
-                cwd: 'models/',
-                src: '**/*',
-                dest: 'builds/models/'
-            }
-            //html in tasks/htmlmin.js
-        ]
-    }
+      //root files
+      {
+        expand: true,
+        cwd: '',
+        src: 'app.js',
+        dest: 'builds/'
+      },
+      {
+        expand: true,
+        cwd: '',
+        src: 'fakes3.sh',
+        dest: 'builds/'
+      },
+      //libs
+      {
+        expand: true,
+        cwd: 'node_modules/',
+        src: '**/lib/*.js',
+        dest: 'builds/node_modules/'
+      },
+      {
+        expand: true,
+        cwd: 'libs/',
+        src: '**/*',
+        dest: 'builds/libs/'
+      },
+      //public
+      //css in tasks/cssmin.js
+      {
+        expand: true,
+        cwd: 'public/images',
+        src: '**/*',
+        dest: 'builds/public/images'
+      },
+      {
+        expand: true,
+        cwd: 'public/js',
+        src: '**/*.js',
+        dest: 'builds/public/js'
+      },
+      //mvc
+      {
+        expand: true,
+        cwd: 'controllers/',
+        src: '**/*',
+        dest: 'builds/controllers/'
+      },
+      {
+        expand: true,
+        cwd: 'models/',
+        src: '**/*',
+        dest: 'builds/models/'
+      }
+      //html in tasks/htmlmin.js
+    ]
+  }
 };
