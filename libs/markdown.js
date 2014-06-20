2b5e93de81b81b3e5c49aa5e66cbed0ce8d7c4c2
Martii <thalamew@hotmail.com>
2014-06-20 03:47:41 -0600

    Move html whitelist to a json file so it doesn't clutter up the main code and read in

--- libs/markdown.js
+++ libs/markdown.js
@@ -1,6 +1,8 @@
 var marked = require('marked');
 var hljs = require('highlight.js');
 var sanitizeHtml = require('sanitize-html');
+var htmlWhitelist = require('./htmlwhitelist.json');
+
 var renderer = new marked.Renderer();
 
 // Automatically generate an anchor for each header
@@ -39,82 +41,6 @@ marked.setOptions({
 });
 
 exports.renderMd = function (text) {
-  return marked(sanitizeHtml(text), {
-    allowedTags: [
-      'h1',
-      'h2',
-      'h3',
-      'h4',
-      'h5',
-      'h6',
-      'h7',
-      'h8',
-      'p',
-      'div',
-      'blockquote',
-      'pre',
-      'b',
-      'i',
-      'strong',
-      'em',
-      'tt',
-      'code',
-      'ins',
-      'del',
-      'sup',
-      'sub',
-      'kbd',
-      'samp',
-      'q',
-      'var',
-      'ol',
-      'ul',
-      'li',
-      'dl',
-      'dt',
-      'dd',
-      'table',
-      'thead',
-      'tbody',
-      'tfoot',
-      'tr',
-      'td',
-      'th',
-      'br',
-      'hr',
-      'ruby',
-      'rt',
-      'rp'
-    ],
-    allowedAttributes: {
-      a: [
-        'href'
-      ],
-      img: [
-        'src'
-      ],
-      div: [
-        'itemscope',
-        'itemtype'
-      ]
-    },
-    // Lots of these won't come up by default because we don't allow them
-    selfClosing: [
-      'img',
-      'br',
-      'hr',
-      'area',
-      'base',
-      'basefont',
-      'input',
-      'link',
-      'meta'
-    ],
-    // URL schemes we permit
-    allowedSchemes: [
-      'http',
-      'https',
-      'mailto'
-    ]
-  });
+  console.log(htmlWhitelist.allowedTags);
+  return marked(sanitizeHtml(text), htmlWhitelist);
 };
