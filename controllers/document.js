36013e4b45896f3721bcee64f70116882acbc513
Martii <thalamew@q.com>
2014-08-18 22:23:28 -0600

    Reorder things a bit for the document mcv
    Applies to #296

--- controllers/document.js
+++ controllers/document.js
@@ -18,6 +18,8 @@ exports.view = function (aReq, aRes, aNext) {
   var matches = null;
   var page = null;
 
+  var documentPath = null;
+
   //---
   function preRender() {};
   function render() { aRes.render('pages/documentPage', options); }
@@ -30,26 +32,14 @@ exports.view = function (aReq, aRes, aNext) {
 
   //--- Tasks
 
-  matches = aReq._parsedUrl.pathname.match(/^\/about\/(.*)$/);
-  if (matches) {
-    page = matches[1];
+  var document = aReq.route.params.document;
 
-    if (page == 'about') {
-      fs.readdir('views/includes/documents', function (aErr, aFiles) {
-        if (aErr) {
-          return statusCodePage(aReq, aRes, aNext, {
-            statusCode: 404,
-            statusMessage: 'Page not found.'
-          });
-        }
+  if (document) {
+    documentPath = 'views/includes/documents';
 
+    fs.readdir(documentPath, function (aErr, aFiles) {
+      if (!aErr) {
         var file = null;
-
-        // Page metadata
-        pageMetadata(options, ['About', 'About']);
-
-        options.isAbout = true;
-        options.pageHeading = 'About';
 
         // Dynamically create a file listing of the pages
         options.files = [];
@@ -61,52 +51,51 @@ exports.view = function (aReq, aRes, aNext) {
             });
           }
         }
+      }
+    });
 
-        async.parallel(tasks, asyncComplete);
-      });
-    }
-    else {
-      fs.readFile('views/includes/documents/' + matches[1] + '.md', 'UTF8', function (aErr, aData) {
-        if (aErr) {
-          return statusCodePage(aReq, aRes, aNext, {
-            statusCode: 404,
-            statusMessage: 'Page not found.'
-          });
-        }
+    fs.readFile(documentPath + '/' + document + '.md', 'UTF8', function (aErr, aData) {
+      if (aErr) {
+        return statusCodePage(aReq, aRes, aNext, {
+          statusCode: 404,
+          statusMessage: 'Page not found.'
+        });
+      }
 
-        var lines = null;
-        var heading = null;
-        var content = null;
+      var lines = null;
+      var heading = null;
+      var content = null;
 
-        // Check if first line is h2 and use for title/heading if present
-        lines = aData.match(/.*/gm);
-        if (lines) {
-          matches = lines[0].match(/^##\s(.*)$/);
-          if (matches) {
-            heading = lines.shift().replace(/^##\s+/, "");
-          } else {
-            heading = page;
-          }
-          content = lines.join('\n');
+      // Check if first line is h2 and use for title/heading if present
+      lines = aData.match(/.*/gm);
+      if (lines) {
+        matches = lines[0].match(/^##\s(.*)$/);
+        if (matches) {
+          heading = lines.shift().replace(/^##\s+/, "");
         } else {
           heading = page;
-          content = aData;
         }
+        content = lines.join('\n');
+      } else {
+        heading = page;
+        content = aData;
+      }
 
-        // Page metadata
-        pageMetadata(options, [heading, 'About']);
+      // Page metadata
+      pageMetadata(options, [heading, 'About']);
 
-        options.pageHeading = heading;
-        options.pageData = renderMd(content);
+      options.pageHeading = heading;
+      options.pageData = renderMd(content);
 
-        async.parallel(tasks, asyncComplete);
-      });
-    }
+      async.parallel(tasks, asyncComplete);
+    });
   }
   else {
-    statusCodePage(aReq, aRes, aNext, {
-      statusCode: 404,
-      statusMessage: 'Page not found.'
-    });
+    // Page metadata
+    pageMetadata(options, ['About', 'About']);
+
+    options.isAbout = true;
+
+    async.parallel(tasks, asyncComplete);
   }
 };
