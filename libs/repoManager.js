ce38eda7a1f6f3a19043c518ae2aca747b3681c7
Martii <thalamew@q.com>
2015-10-05 15:04:03 -0600

    Try lower-case utf-16 full to see what GH does with it
    Applies to #678

--- libs/repoManager.js
+++ libs/repoManager.js
@@ -34,7 +34,7 @@ function fetchRaw(aHost, aPath, aCallback) {
       'User-Agent': 'Node.js',
 
       // See #678
-      'Accept': 'text/plain; charset=UTF-16'
+      'Accept': 'text/plain; charset=utf-16'
     }
   };
 
