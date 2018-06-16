532733af71cb3feaf5026ea5ecb89fe621a60563
Martii <martii@users.noreply.github.com>
2018-06-16 01:46:24 -0600

    Delete lockfile instead of global ignore
    * Will allow audit'ing again

---
+++ dev/preinit.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Define some pseudo module globals
+var isPro = require('../libs/debug').isPro;
+var isDev = require('../libs/debug').isDev;
+var isDbg = require('../libs/debug').isDbg;
+
+//
+// NOTE: Only use native *node* `require`s in this file
+//   since dependencies may not be installed yet
+//
+var fs = require('fs');
+
+console.log('Attempting to delete `package-lock.json`');
+fs.unlinkSync('./package-lock.json');
