f2b6c143f091167d264f72c2be4e3ab4c11d7f97
Martii <martii@users.noreply.github.com>
2025-03-04 12:44:52 -0500

    Revert "Create pattern blob for indexing (#2060)"
    This reverts commit 4463468f0462b0622bf0cdd2483e7c41537bbde0.
    
    Not the issue... reversion of #2060 and post #2048

--- libs/modelQuery.js
+++ libs/modelQuery.js
@@ -125,8 +125,8 @@ var parseScriptSearchQuery = function (aScriptListQuery, aQuery, aLimited) {
     });
   } else {
     parseModelListSearchQuery(aScriptListQuery, aQuery, {
-      partialWordMatchFields: ['name', 'author', '_description', '_about'],
-      fullWordMatchFields: ['_pattern']
+      partialWordMatchFields: ['name', '_description', 'author', '_about' ],
+      fullWordMatchFields: ['meta.UserScript.include.value', 'meta.UserScript.match.value']
     });
   }
 };
