8bd413bb98dd71a60310ebde32d57d6787e853b2
Chris-C1108 <chen.m1108@gmail.com>
2026-09-19 10:47:46 +0800

    Use repo detected default_branch for importing (#1781)

--- controllers/user.js
+++ controllers/user.js
@@ -1658,11 +1658,6 @@ exports.userGitHubRepoPage = function (aReq, aRes, aNext) {
       },
       function (aRepo, aCallback) {
         options.repo = aRepo;
-        if (authedUser && authedUser.ghBranch) {
-          options.repo.default_branch = authedUser.ghBranch;
-        } else {
-          options.repo.default_branch = options.repo.default_branch || 'master';
-        }
         options.repoAsEncoded = {
           default_branch: encodeURIComponent(options.repo.default_branch)
         };
@@ -1747,8 +1742,7 @@ exports.userGitHubImportScriptPage = function (aReq, aRes, aNext) {
 
   options.githubRepoName = githubRepoName = aReq.body.repo || aReq.query.repo;
   options.githubDefaultBranch = githubDefaultBranch =
-    (authedUser && authedUser.ghBranch) || aReq.body.default_branch || aReq.query.default_branch
-    || 'master';
+    aReq.body.default_branch || aReq.query.default_branch;
   options.githubPathName = githubPathName = aReq.body.pathname || aReq.query.pathname;
   options.githubPathExt = githubPathExt = aReq.body.pathext || aReq.query.pathext;
   options.githubBlobPath = githubBlobPath = aReq.body.path || aReq.query.path;
